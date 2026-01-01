40b87540e19c7e31df3cd1b40e5b4bacfb74598b
Danieldoti <danizchrist@gmail.com>
2026-01-01 17:09:36 +0200

    cleaning update completed order api

--- server.js
+++ server.js
@@ -172,18 +172,23 @@ app.post("/api/confirm-checkout", upload.single("paymentProof"), async (req, res
 // ✅ Update Status
 app.put("/api/orders/:id", verifyAdmin, async (req, res) => {
   try {
-    const order = await Order.findOne({ "customer.id": req.params.id.trim() });
+    const order = await Order.findById(req.params.id.trim());
     if (!order) return res.status(404).json({ message: "Order not found" });
 
-    if (order.status === "Pending Payment Invoice") order.status = "Completed";
-    else if (order.status === "Completed") order.status = "Deleted";
+    if (order.status === "Pending Payment Invoice") {
+      order.status = "Completed";
+    } else if (order.status === "Completed") {
+      await order.deleteOne();
+      return res.json({ message: "Order deleted" });
+    }
 
     await order.save();
-    res.json({ message: `Status: ${order.status}`, order });
+    res.json({ message: `Status updated`, order });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
+
 
 app.post("/get-account", async (req, res) => {
   try {
