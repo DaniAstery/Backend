af1d31df0aafb1924127f1b1dda60e943cde2cbc
Danieldoti <danizchrist@gmail.com>
2026-01-21 17:54:58 +0200

    passing resend api3 added .env config and cors requireents

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
